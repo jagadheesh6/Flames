d8488f37eb9f573bfaaed7577b8e6e59cd7183b2
Jaga <58385390+jagadheesh6@users.noreply.github.com>
2019-12-30 13:39:50 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$fname = "";
+$sname = "";
+$flames="";
+$errors = array();
+ $con = mysqli_connect('sql204.epizy.com', 'epiz_24802663', 'sslmdraj', 'epiz_24802663_flames');
+$db = mysqli_connect('sql204.epizy.com', 'epiz_24802663', 'sslmdraj', 'epiz_24802663_flames');
+if(!$con)
+{
+    die('please check u r connection'.mysqli_error($con));
+}
+if (isset($_POST['flame'])) {
+    $fname = mysqli_real_escape_string($db, $_POST['fname']);
+    $sname = mysqli_real_escape_string($db, $_POST['sname']);
+    if (empty($fname)) { array_push($errors, "Name is required"); }
+  if (empty($sname)) { array_push($errors, "Name is required"); }
+  if (count($errors) == 0) {
+    $sql="INSERT INTO flame(fname,sname) VALUES ('$fname','$sname')";  
+    mysqli_query($db,$sql); 
+  }
+}
+
+?>
